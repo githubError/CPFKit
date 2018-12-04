abf73145de85a7501e66c8e32fc2647df78c5a27
崔鹏飞 <cuipengfei@entgroup.com>
2018-12-04 17:01:20 +0800

    0.0.2

--- CPFKit/Classes/CPFKit.h
+++ CPFKit/Classes/CPFKit.h
@@ -10,6 +10,19 @@
 
 #import "CPFRuntimeKit.h"
 #import "CPFRunloopTaskManager.h"
+#import "CPFFrameLayout.h"
 #import "UIView+CPFAdd.h"
+#import "NSArray+CPFAdd.h"
+#import "NSData+CPFAdd.h"
+#import "NSDate+CPFAdd.h"
+#import "NSDictionary+CPFAdd.h"
+#import "NSString+CPFAdd.h"
+#import "NSString+Judge.h"
+#import "UIApplication+CPFAdd.h"
+#import "UIButton+CPFAdd.h"
+#import "UIColor+CPFAdd.h"
+#import "UIDevice+CPFAdd.h"
+#import "UIImage+CPFAdd.h"
+#import "UINavigationController+CPFNavigationBarAlpha.h"
 
 #endif /* CPFKit_h */
